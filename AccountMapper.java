48ca4fe148cc2449aa9192574e09f1fe0d61445c
jaysara <32116587+jaysara@users.noreply.github.com>
2025-01-02 20:10:28 -0600

    Update AccountMapper.java

--- AccountMapper.java
+++ AccountMapper.java
@@ -1,70 +1,150 @@
-package com.test.filecompare;
+package com.test.cobol;
 
+import java.lang.reflect.Field;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 
 public class AccountMapper {
 
-    public static DestAccount mapAccountToDestAccount(Account account) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+    public static void mapSrcToDestJson(SrcAccount src, DestAccount dest) {
+        Class<?> srcClass = src.getClass();
+        Class<?> destClass = dest.getClass();
 
-        // Convert Account to JSON
-        String json = mapper.writeValueAsString(account);
+        Field[] srcFields = srcClass.getDeclaredFields();
 
-        // Convert JSON to DestAccount
-        DestAccount destAccount = mapper.readValue(json, DestAccount.class);
+        for (Field srcField : srcFields) {
+            srcField.setAccessible(true);
+            try {
+                // Get the @JsonProperty annotation value
+                JsonProperty annotation = srcField.getAnnotation(JsonProperty.class);
+                if (annotation != null) {
+                    String destFieldName = annotation.value();
 
-        return destAccount;
+                    Field destField = destClass.getDeclaredField(destFieldName);
+                    destField.setAccessible(true);
+
+                    Object value = srcField.get(src);
+                    destField.set(dest, value);
+                }
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                System.out.println("Mapping failed for field: " + srcField.getName());
+            }
+        }
+    }
+    public static void mapSrcToDest(SrcAccount src, DestAccount dest) {
+        Class<?> srcClass = src.getClass();
+        Class<?> destClass = dest.getClass();
+
+        Field[] srcFields = srcClass.getDeclaredFields();
+
+        for (Field srcField : srcFields) {
+            srcField.setAccessible(true);
+            try {
+                String srcFieldName = srcField.getName();
+                String destFieldName = camelToSnake(srcFieldName);
+
+                Field destField = destClass.getDeclaredField(destFieldName);
+                destField.setAccessible(true);
+
+                Object value = srcField.get(src);
+                destField.set(dest, value);
+
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                System.out.println("Mapping failed for field: " + srcField.getName());
+            }
+        }
     }
 
-    public static void main(String[] args) throws JsonProcessingException {
-        // Create a sample Account object
-        Account account = new Account();
-        account.setAccountId(123456);
-        account.setEndDate("2023-06-30");
-        account.setEcoaCde("ABC");
+    private static String camelToSnake(String camelCase) {
+        return camelCase.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
+    }
 
-        // Map Account to DestAccount
-        DestAccount destAccount = mapAccountToDestAccount(account);
+    public static void main(String[] args) {
+        // Example usage
+        SrcAccount src = new SrcAccount();
+        src.setAccountId(12345);
+        src.setEndDate("2025-01-01");
+        src.setEcoaCde("EC01");
 
-        // Print the mapped DestAccount
-        System.out.println("Mapped DestAccount:");
-        System.out.println("Account ID: " + destAccount.getAccountId());
-        System.out.println("End Date: " + destAccount.getEndDate());
-        System.out.println("ECOA Code: " + destAccount.getEcoaCde());
+        DestAccount dest = new DestAccount();
+        mapSrcToDestJson(src, dest);
+
+        System.out.println(dest);
     }
 }
 
-class Account {
+// Source Account POJO
+class SrcAccount {
     @JsonProperty("acct_id")
     private long accountId;
+
     @JsonProperty("end_dte")
     private String endDate;
+
     @JsonProperty("ecoa_cde")
     private String ecoaCde;
 
-    // Getters and setters
-    public long getAccountId() { return accountId; }
-    public void setAccountId(long accountId) { this.accountId = accountId; }
-    public String getEndDate() { return endDate; }
-    public void setEndDate(String endDate) { this.endDate = endDate; }
-    public String getEcoaCde() { return ecoaCde; }
-    public void setEcoaCde(String ecoaCde) { this.ecoaCde = ecoaCde; }
+    public long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEcoaCde() {
+        return ecoaCde;
+    }
+
+    public void setEcoaCde(String ecoaCde) {
+        this.ecoaCde = ecoaCde;
+    }
 }
 
+// Destination Account POJO
 class DestAccount {
-    private long accountId;
-    private String endDate;
-    private String ecoaCde;
+    private long acct_id;
+    private String end_dte;
+    private String ecoa_cde;
 
-    // Getters and setters
-    public long getAccountId() { return accountId; }
-    public void setAccountId(long accountId) { this.accountId = accountId; }
-    public String getEndDate() { return endDate; }
-    public void setEndDate(String endDate) { this.endDate = endDate; }
-    public String getEcoaCde() { return ecoaCde; }
-    public void setEcoaCde(String ecoaCde) { this.ecoaCde = ecoaCde; }
+    public long getAcct_id() {
+        return acct_id;
+    }
+
+    public void setAcct_id(long acct_id) {
+        this.acct_id = acct_id;
+    }
+
+    public String getEnd_dte() {
+        return end_dte;
+    }
+
+    public void setEnd_dte(String end_dte) {
+        this.end_dte = end_dte;
+    }
+
+    public String getEcoa_cde() {
+        return ecoa_cde;
+    }
+
+    public void setEcoa_cde(String ecoa_cde) {
+        this.ecoa_cde = ecoa_cde;
+    }
+
+    @Override
+    public String toString() {
+        return "DestAccount{" +
+                "acct_id=" + acct_id +
+                ", end_dte='" + end_dte + '\'' +
+                ", ecoa_cde='" + ecoa_cde + '\'' +
+                '}';
+    }
 }
+
